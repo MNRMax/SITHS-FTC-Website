266edc08086bf156c5813250248bb7cff0ca375e
MNRMax <maximilian.shabatayev@gmail.com>
2024-01-21 18:01:24 -0500

    redo the whole thing

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="h-[40vh]">
+
+    </div>
+    <div class="bg-[#e6e1f4] w-1/2 m-auto rounded-[1rem] text-center text-[3rem] font-[Orange-Milk] text-[#0042c7]">
+        <h1 class="">Staten Island Tech's FTC Team</h1>
+        <h1 class="">SITHS Mercury 23463</h1>
+    </div>
+    <div class="h-[35vh]">
+
+    </div>
+    <div id="scroll-text" class="w-full text-center text-[2rem]">
+        <h2 class="font-[Cocogoose]">Scroll Down</h2>
+        <span id="down" class="material-symbols-outlined">
+            expand_more
+        </span>
+    </div>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+onMounted(() => {
+    gsap.to("#down", {
+        y: 10,
+        repeat: -1,
+        duration: 1,
+        yoyo: true,
+    })
+    gsap.to("#scroll-text", {
+        y: -100,
+        opacity: 0,
+        duration: 1,
+        yoyo: true,
+        scrollTrigger: {
+            trigger: "#scroll-text",
+            start: "bottom center",
+            scrub: true,
+        }
+    })
+});
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="home" class="w-[100%] h-[100vh]">
+        <HomeComponent/>
+    </div>
+    <div id="mission" class="w-[100%] h-[100vh]">
+        <MissionComponent/>
+    </div>
+    <div id="team" class="w-[100%] h-[100vh]">
+        <TeamComponent/>
+    </div>
+</template>
+
+<script setup>
+import { gsap } from "gsap";
+
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import HomeComponent from "@/components/HomeComponent.vue";
+import MissionComponent from "@/components/MissionComponent.vue";
+import { onMounted } from "vue";
+import TeamComponent from "@/components/TeamComponent.vue";
+
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+
+onMounted(() => {
+    gsap.to("#home", {
+        ease: "none",
+        scrollTrigger: {
+            trigger: "#mission",
+            delay: 0.1,
+            durration: 0.01,
+            snap: 0.5,
+        }
+    })
+});
+
+</script>
